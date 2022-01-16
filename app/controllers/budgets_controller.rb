d22ce86fbc2fb12d27f0a36b498fb8740e9b8a27
yakaaaan11 <ayasax.0611@gmail.com>
2022-01-16 10:20:05 +0000

    [Add]予算分析ページ

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,6 +1,17 @@
 class BudgetsController < ApplicationController
   def index
+    @today = Date.today
+    @pays_month = current_user.pays.where(date: @today.all_month)
+    @pays_month_ratio = @pays_month.group(:category_id).sum(:price).sort_by {|_,v|v}.reverse.to_h
+    @pays_month_total = @pays_month.inject(0) {|sum,pay| sum + pay.price}
+
     @budgets = current_user.budgets.all
+    @budgets_total = @budgets.inject(0) {|sum,budget| sum + budget.price}
+    @budget_month_ratio = @pays_month_total*100 / @budgets_total
+
+    @category_parent_array = Category.where(ancestry: nil)
+
+
   end
 
   def new
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,15 @@ class Category < ApplicationRecord
   has_many :budgets
   belongs_to :user
 
+
+  # 予算残高：予算ー支出
+  def budgets_balance
+    budgets.sum(:price) - pays.sum(:price)
+  end
+
+  def budgets_ratio
+    pays.sum(:price)*100 / budgets.sum(:price)
+  end
+
+
 end
