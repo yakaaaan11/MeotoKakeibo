ec576bf0565ca1b9031e5af3b83d7429f19c997b
yakaaaan11 <ayasax.0611@gmail.com>
2022-01-22 11:26:54 +0000

    [Update]予算分析一覧

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -14,8 +14,10 @@ class BudgetsController < ApplicationController
     @budgets = current_user.budgets.all
     @budgets_total = @budgets.sum(:price)
     @budget_month_ratio = @pays_month_total*100 / @budgets_total
+    @budget_month_difference = @pays_month_total - @budgets_total
 
-    @category_parent_array = Category.where(ancestry: nil)
+    @parent_categories = Category.roots
+
   end
 
   def new
